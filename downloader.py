a3c28b163524824d3fe9afb7929cfd7656895b6a
mcoder01 <manu01141@gmail.com>
2026-01-28 01:28:10 +0100

    Improved performances and refactored code

--- downloader.py
+++ downloader.py
@@ -1,39 +1,36 @@
-from concurrent.futures import ThreadPoolExecutor
 import os
-
 import yt_dlp
 import requests
+
+from concurrent.futures import ThreadPoolExecutor
 from PyQt6.QtGui import QPixmap
 
-temp_folder = os.environ.get("TEMP") or os.environ.get("TMP") \
-    or os.environ.get("TMPDIR") or "/tmp"
-
 class Downloader:
-    def __init__(self, max_workers=os.cpu_count()):
+    def __init__(self, max_workers=8):
         self.executor = ThreadPoolExecutor(max_workers)
         self.downloads = {}
 
-    def submit(self, func, **kwargs):
+    def __submit(self, func, **kwargs):
         return self.executor.submit(func, **kwargs)
 
-    def progress_callback(self, d):
+    def __progress_callback(self, d):
         if d['status'] == 'downloading':
             downloaded = d.get('downloaded_bytes', 0)
             total = d.get('total_bytes') or d.get('total_bytes_estimate')
 
             if total:
-                percent = downloaded / total * 100
+                percent = downloaded / total * 99.9
                 progress_str = f"{percent:.1f}%"
             else:
                 progress_str = f"{downloaded / 1E6:.1f} MB"
 
             link = d['info_dict']['webpage_url']
-            self.downloads[link].progress.setText(progress_str)
+            self.downloads[link].progressSignal.emit(progress_str)
 
-    def download_video(self, link, dest):
+    def __download_video(self, link, dest):
         ydl_opts = {
             'format': 'bestaudio/best',
-            'progress_hooks': [self.progress_callback],
+            'progress_hooks': [self.__progress_callback],
             'outtmpl': os.path.join(dest, '%(title)s.%(ext)s'),
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
@@ -47,33 +44,29 @@ class Downloader:
     
         with yt_dlp.YoutubeDL(ydl_opts) as ydl: 
             ydl.download([link])
+        self.downloads[link].progressSignal.emit("100.0%")
 
-    def show_info(self, link, info, widget):
-        thumbnail_url = info.get('thumbnail')
-        if thumbnail_url:
-            data = requests.get(thumbnail_url).content
-            thumbnail = QPixmap()
-            thumbnail.loadFromData(data)
-            widget.thumbnail.setPixmap(thumbnail)
-        widget.title.setText(info.get("title", link))
-        widget.title.setStyleSheet("")
+    def __download_thumbnail(self, url, signal):
+        data = requests.get(url).content
+        thumbnail = QPixmap()
+        thumbnail.loadFromData(data)
+        signal.emit(thumbnail)
 
-    def get_info(self, link, widget):
+    def get_thumbnail(self, url, signal):
+        self.__submit(self.__download_thumbnail, url=url, signal=signal)
+
+    def __download_info(self, link, signal):
         ydl_opts = {'noplaylist': True, 'ignoreerrors': True, 'quiet': True}
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(link, download=False)
-            self.show_info(link, info, widget)
+        signal.emit(info)
+
+    def get_info(self, link, signal):
+        self.__submit(self.__download_info, link=link, signal=signal)
         
-    def submit_video(self, link, output_folder, widget, info=None):
-        if link in self.downloads:
-            self.submit(self.download_video, link=link, dest=output_folder)
-        else:
-            self.downloads[link] = widget
-            self.submit(self.download_video, link=link, dest=output_folder)
-            if info is None:
-                self.submit(self.get_info, link=link, widget=widget)
-            else:
-                self.show_info(link, info, widget)
+    def submit_video(self, link, output_folder, widget):
+        self.downloads[link] = widget
+        self.__submit(self.__download_video, link=link, dest=output_folder)
 
     def shutdown(self):
         self.executor.shutdown(wait=False)
--- youtube2mp3.py
+++ youtube2mp3.py
@@ -2,11 +2,14 @@ import os
 import sys
 
 from PyQt6.QtWidgets import QMainWindow, QApplication, QGraphicsOpacityEffect, QFileDialog, QWidget
-from PyQt6.QtGui import QIcon
+from PyQt6.QtGui import QIcon, QPixmap
+from PyQt6.QtCore import Qt, pyqtSignal
 from PyQt6 import uic
 
 import yt_dlp
 from downloader import Downloader
+
+downloader = Downloader()
 
 def setVisible(widget, visible):
     effect = widget.graphicsEffect()
@@ -17,18 +20,39 @@ def setVisible(widget, visible):
     widget.setGraphicsEffect(effect)
 
 class DlItem(QWidget):
-    def __init__(self, **kwargs):
+    infoSignal = pyqtSignal(dict)
+    progressSignal = pyqtSignal(str)
+    thumbnailSignal = pyqtSignal(QPixmap)
+
+    def __init__(self, link, **kwargs):
         super().__init__(**kwargs)
         uic.loadUi("list_item.ui", self)
 
+        self.link = link
+        self.infoSignal.connect(self.show_info)
+        self.progressSignal.connect(self.progress.setText)
+        self.thumbnailSignal.connect(self.show_thumbnail)
+
+    def show_info(self, info):
+        thumbnail_url = info.get('thumbnail')
+        if thumbnail_url:
+            downloader.get_thumbnail(thumbnail_url, self.thumbnailSignal)
+        self.title.setText(info.get("title", self.link))
+        self.title.setStyleSheet("")
+
+    def show_thumbnail(self, thumbnail):
+        self.thumbnail.setPixmap(thumbnail)
+
 class Converter(QMainWindow):
+    urlInfoSignal = pyqtSignal(dict)
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         uic.loadUi("gui.ui", self)
 
         setVisible(self.dlSection, False)
         self.output_folder = os.path.join(os.environ['USERPROFILE'], 'Music')
-        self.downloader = Downloader()
+        self.urlInfoSignal.connect(self.start_conversions)
 
     def chooseFolder(self):
         folder = QFileDialog.getExistingDirectory(self, "Select the download folder...", self.output_folder)
@@ -37,31 +61,39 @@ class Converter(QMainWindow):
 
     def convert_video(self, id, info=None):
         link = f"https://www.youtube.com/watch?v={id}"
-        if link in self.downloader.downloads:
-            item = self.downloader.downloads[link]
+        if link in downloader.downloads:
+            item = downloader.downloads[link]
             item.progress.setText("0.0%")
         else:
-            item = DlItem()
+            item = DlItem(link)
             self.dlList.layout().insertWidget(0, item)
-        self.downloader.submit_video(link, self.output_folder, item, info)
+
+        if info:
+            item.show_info(info)
+        else:
+            downloader.get_info(link, item.infoSignal)
+        downloader.submit_video(link, self.output_folder, item)
+
+    def start_conversions(self, info):
+        if info.get('_type') == "playlist":
+            for entry in info['entries']:
+                if entry.get("id"):
+                    self.convert_video(entry['id'])
+        else:
+            self.convert_video(info['id'], info)
+        self.convertBtn.setEnabled(True)
+        QApplication.restoreOverrideCursor()
 
     def convert(self):
         link = self.link.text()
         if len(link) == 0:
             return
         
+        self.convertBtn.setEnabled(False)
+        QApplication.setOverrideCursor(Qt.CursorShape.WaitCursor)
         os.makedirs(self.output_folder, exist_ok=True)
         setVisible(self.dlSection, True)
-
-        ydl_opts = {'ignoreerrors': True, 'quiet': True}
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(link, download=False)
-            if info.get('_type') == "playlist":
-                for entry in info['entries']:
-                    if entry.get("id"):
-                        self.convert_video(entry['id'])
-            else:
-                self.convert_video(info['id'], info)
+        downloader.get_info(link, self.urlInfoSignal)
 
 if __name__ == "__main__":
     try:
